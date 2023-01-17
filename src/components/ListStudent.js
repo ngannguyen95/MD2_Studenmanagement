1e1ecdfc6a0854c1c48a2d510c5a782b26a75e65
Nguyencongnguyen1812 <ngannt33@wru.vn>
2023-01-17 10:01:02 +0700

    ghghh

---
+++ src/components/ListStudent.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Student from './Student';
+
+class ListStudent extends Component {
+  render() {
+    return (
+      <div>
+        <div className="card-body">
+          <h3 className="card-title">Danh sách sinh viên</h3>
+          <div className="table-responsive pt-3">
+            <table className="table table-bordered">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Mã sinh viên</th>
+                  <th>Tên sinh viên</th>
+                  <th>Tuổi</th>
+                  <th>Giới tính</th>
+                  <th>Hành động</th>
+                </tr>
+              </thead>
+              <tbody>
+               <Student></Student>
+               <Student></Student>
+               <Student></Student>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+export default ListStudent;
